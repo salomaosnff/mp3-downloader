ce99a5e0f9b25e95dabdf21bc644738674d9d488
Salomão Neto <salomaosnff3@gmail.com>
2019-05-13 14:55:49 -0300

    MusicPleer Corrigido

--- providers/musicpleer.js
+++ providers/musicpleer.js
@@ -3,7 +3,7 @@ const cheerio = require('cheerio')
 const puppeeter = require('puppeteer')
 
 class MusicPleerProvider extends DownloadProvider {
-  constructor () {
+  constructor() {
     super({
       id: 'musicpleer',
       name: 'MusicPleer'
@@ -22,37 +22,41 @@ class MusicPleerProvider extends DownloadProvider {
     })
   }
 
-  async search (query) {
+  async search(query) {
     const browser = await this.browser
     const page = await browser.newPage()
     await page.setRequestInterception(true);
 
     page.on('request', (req) => {
-      if(['image', 'font', 'stylesheet'].includes(req.resourceType())){
-          req.abort();
+      if (['image', 'font', 'stylesheet'].includes(req.resourceType())) {
+        req.abort();
       } else {
         req.continue();
       }
     })
 
-    await page.goto(`https://musicpleer.media/#!${query}`)
-    await page.waitFor('#searchResults')
-    const results = await page.$$('#searchResults ul li')
+    try {
+      await page.goto(`https://musicpleer.media/#!${query}`)
+      await page.waitFor('#searchResults')
+      const results = await page.$$('#searchResults ul li')
 
-    return Promise.all(
-      results.map(async e => {
-        const title = await e.$eval('a h3', e => e.textContent)
-        const artist = (await e.$eval('a p', e => e.textContent)).replace('Artist: ', '')
-        return {
-          title: `${title} - ${artist}`,
-          link : await e.$eval('a', e => e.href),
-          page,
-        }
-      })
-    )
+      return Promise.all(
+        results.map(async e => {
+          const title = await e.$eval('a h3', e => e.textContent)
+          const artist = (await e.$eval('a p', e => e.textContent)).replace('Artist: ', '')
+          return {
+            title: `${title} - ${artist}`,
+            link: await e.$eval('a', e => e.href),
+            page,
+          }
+        })
+      )
+    } catch (e) {
+      return []
+    }
   }
 
-  async resolve (item) {
+  async resolve(item) {
     await item.page.goto(item.link)
     await item.page.waitFor('#download-btn')
     const link = await item.page.$eval('#download-btn', e => e.href)
@@ -60,7 +64,7 @@ class MusicPleerProvider extends DownloadProvider {
     return link
   }
 
-  async destroy () {
+  async destroy() {
     const browser = await this.browser
     await browser.close()
     return null
